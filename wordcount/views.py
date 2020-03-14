5cf230ebb9ceb98312bddc0717ff3f45f4c09c1d
skim <sam.kim@tobii.co.jp>
2020-03-14 21:14:59 +0900

    all

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 def homepage(request):
     return render(request, 'home.html', {'hithere': 'This is me'})
@@ -10,4 +11,15 @@ def eggs(request):
 def count(request):
     name = request.GET['name']
     wordlist = name.split()
-    return render(request, 'count.html', {'name': name, 'count': len(wordlist)})
+    worddict = {}
+    for word in wordlist:
+        if word in worddict:
+            #increase
+            worddict[word] += 1
+        else:
+            #add to the dict
+            worddict[word] = 1
+
+    sortedwords = sorted(worddict.items(), key=operator.itemgetter(1), reverse=True)
+
+    return render(request, 'count.html', {'name': name, 'count': len(wordlist), 'sortedwords': sortedwords})
